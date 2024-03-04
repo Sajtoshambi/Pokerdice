5dca7b4ef1159a7fe9a065ee530bbdabed4c97a4
Sajtoshambi <120171390+Sajtoshambi@users.noreply.github.com>
2024-03-04 09:39:24 +0100

    pokerdice.py pokerdicemain.py

--- pokerdice.py
+++ pokerdice.py
@@ -5,10 +5,25 @@ def Pokerdice:
 
 
     def dobas(self):
-        self.dice = 0
-        [random.randint(1, 6)] for i in range(5):]
+        self.dice = [random.randint(1, 6)] for i in range(5):]
 
 
     def kocka(self):
         print("Kocka", self.dice)
+
+    def checkhand(self):
+        e_érték=(
+set(self.dice))
+        if len(e_érték)==1:
+            return "5 ugyanolyan"
+        elif len(e_érték)==2
+            for value in e_érték:
+         if self.dice.count(value)==4:
+            return ("4 ugyanolyan"
+                            "Full Haus")
+         elif len(e_érték)==5:
+             return print("High card")
+         else:
+             print("Érvénytelen leosztás")
+
 
