07a8699530450f4b9f837ead92202d32fd3e2f7a
Sajtoshambi <120171390+Sajtoshambi@users.noreply.github.com>
2024-03-04 09:17:57 +0100

    pokerdice.py pokerdicemain.py

--- pokerdice.py
+++ pokerdice.py
@@ -1,1 +1,14 @@
-class pker:
+import random
+def Pokerdice:
+    def __init__(self):
+        self.dice =[1,2,3,4,5]
+
+
+    def dobas(self):
+        self.dice = 0
+        [random.randint(1, 6)] for i in range(5):]
+
+
+    def kocka(self):
+        print("Kocka", self.dice)
+
